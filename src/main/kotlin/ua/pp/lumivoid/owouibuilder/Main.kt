55e3f49977784b47090d64fa9b8d9836e279af76
Artem <krutog727@gmail.com>
2024-06-16 18:18:34 +0300

    add icon

--- src/main/kotlin/ua/pp/lumivoid/owouibuilder/Main.kt
+++ src/main/kotlin/ua/pp/lumivoid/owouibuilder/Main.kt
@@ -3,14 +3,16 @@ package ua.pp.lumivoid.owouibuilder
 import javafx.application.Application
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
+import javafx.scene.image.Image
 import javafx.stage.Stage
 
 class Main : Application() {
     override fun start(stage: Stage) {
-        val fxmlLoader = FXMLLoader(this.javaClass.getResource("views/main-view.fxml"))
+        val fxmlLoader = FXMLLoader(javaClass.getResource("views/main-view.fxml"))
         val scene = Scene(fxmlLoader.load(), 320.0, 240.0)
         stage.title = "OwO UI Builder"
         stage.scene = scene
+        stage.icons.add(Image(javaClass.getResourceAsStream("icons/owo.png")))
         stage.show()
     }
 }
